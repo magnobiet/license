d7d814de1299022ca0a1d9f39c275791cce8a802
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2023-01-22 17:59:31 -0300

    chore: add Tailwind CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,15 @@
-import './globals.css'
+import { ReactNode } from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import './globals.css';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+
+      <body className="antialiased bg-slate-900 text-white">
+        <div className="w-full h-screen flex">{children}</div>
+      </body>
     </html>
-  )
+  );
 }
