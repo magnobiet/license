b85e338a849d6348df2f4645222b025e16d357e2
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2023-01-22 18:40:45 -0300

    chore: add default Next headers

--- next.config.js
+++ next.config.js
@@ -10,8 +10,47 @@ const nextConfig = {
         destination: '/mit',
         permanent: true,
       },
-    ]
+    ];
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        // Security Headers based on: https://nextjs.org/docs/advanced-features/security-headers
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
