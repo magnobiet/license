0d5d2850317939677705b3bc7f9b74dad92b88b8
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2023-01-22 18:00:56 -0300

    chore: redirect / to /mit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/mit',
+        permanent: true,
+      },
+    ]
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
